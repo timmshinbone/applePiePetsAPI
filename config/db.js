3d50f513a88af3e265dbb9d048e3bcf653d6cd10
Timm Schoenborn <timmschoenborn@gmail.com>
2024-02-02 14:59:28 -0500

    adds pet model and seed script

--- config/db.js
+++ config/db.js
@@ -1,13 +1,22 @@
 'use strict'
+// allow for grabbing stuff from env
+require('dotenv').config()
 
 // creating a base name for the mongodb
 // REPLACE THE STRING WITH YOUR OWN DATABASE NAME
-const mongooseBaseName = 'applePiePetsApp'
+// const mongooseBaseName = 'applePiePetsApp'
 
 // create the mongodb uri for development and test
+// this code is for a local instance of mongodb
+// const database = {
+// 	development: `mongodb://localhost/${mongooseBaseName}-development`,
+// 	test: `mongodb://localhost/${mongooseBaseName}-test`,
+// }
+
+// This code is for an atlas instance of mongodb
 const database = {
-	development: `mongodb://localhost/${mongooseBaseName}-development`,
-	test: `mongodb://localhost/${mongooseBaseName}-test`,
+	development: process.env.MONGODB_URI,
+	test: process.env.MONGODB_URI,
 }
 
 // Identify if development environment is test or development
