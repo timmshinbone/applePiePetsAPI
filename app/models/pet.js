b42ef85bc89ea8bdf7ef9f4982888b2b6f90c5a0
Timm Schoenborn <timmschoenborn@gmail.com>
2024-02-02 16:04:45 -0500

    tested routes for pets and added virtuals

--- app/models/pet.js
+++ app/models/pet.js
@@ -26,7 +26,31 @@ const petSchema = new mongoose.Schema(
 	},
 	{
 		timestamps: true,
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true },
 	}
 )
+
+// we define virtuals outside of the schema
+
+// virtuals allow us to derive additional data from already existing data on our documents
+// when a document is retrieved, and turned into an object or JSON, the virtuals are added and sent along with the response.
+
+// first virtual, 'full title' - this produces a string, that is derived from the name and type fields
+
+petSchema.virtual('fullTitle').get(function () {
+    return `${this.name} the ${this.type}`
+})
+
+// this second virtual, will determine if the pet is a baby depending on their age
+petSchema.virtual('isABaby').get(function () {
+    if (this.age < 5) {
+        return "Yeah, theyre just a baby"
+    } else if (this.age >= 5 && this.age < 10) {
+        return "Not really a baby, but like, still a baby"
+    } else {
+        return "A good old pet(definitely still a baby)"
+    }
+})
 
 module.exports = mongoose.model('Pet', petSchema)
--- app/routes/pet_routes.js
+++ app/routes/pet_routes.js
@@ -45,7 +45,7 @@ router.get('/pets', (req, res, next) => {
 
 // SHOW
 // GET /pets/5a7db6c74d55bc51bdf39793
-router.get('/pets/:id', requireToken, (req, res, next) => {
+router.get('/pets/:id', (req, res, next) => {
 	// req.params.id will be set based on the `:id` in the route
 	Pet.findById(req.params.id)
 		.then(handle404)
